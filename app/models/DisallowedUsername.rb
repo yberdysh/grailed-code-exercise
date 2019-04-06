93ebadb2a8a2e4963b8cf9b3fff1d935c547f836
Jane Costa <yberdysh@gmail.com>
2019-04-05 21:12:24 -0400

    basic functionality complete

--- app/models/DisallowedUsername.rb
+++ app/models/DisallowedUsername.rb
@@ -3,4 +3,5 @@ class DisallowedUsername < ActiveRecord::Base
   def self.disallowed_usernames
     self.all.map{|username| username.invalid_username}
   end
+
 end
--- app/models/User.rb
+++ app/models/User.rb
@@ -1,31 +1,52 @@
 require 'set'
 
 class User < ActiveRecord::Base
+  UNIQUE_USERNAMES = Set.new
+  # using set to improve time complexity since sets have direct lookup
+
+  def self.find_users_with_disallowed
+    User.all.select{|user| user.check_if_disallowed}
+  end
+
+  def self.resolve_disallowed(dry_run = nil)
+    affected_rows = []
+    names_to_fix = self.find_users_with_disallowed
+    names_to_fix.each do |user|
+      new_name = user.resolve_collision
+      user.username = new_name
+      affected_rows << user
+      UNIQUE_USERNAMES.add(new_name)
+    end
+  end
 
   def self.resolve_collisions(dry_run = nil)
-    unique_usernames = Set.new
     affected_rows = []
     self.all.each do |user|
-      if !unique_usernames.include?(user.username)
-        unique_usernames.add(user.username)
+      if !UNIQUE_USERNAMES.include?(user.username)
+        UNIQUE_USERNAMES.add(user.username)
       else
-        core_name = user.username
-        num = 1
-        while unique_usernames.include?("#{core_name}#{num}")
-          num += 1
-        end
-        # reassign username here
-        user.username = "#{core_name}#{num}"
+        new_name = user.resolve_collision
+        user.username = new_name
         affected_rows << user
-        unique_usernames.add("#{core_name}#{num}")
+        UNIQUE_USERNAMES.add(new_name)
       end
     end
     affected_rows
   end
 
-  # to do another way:
-  #   my_string = 'potato8585'
-  # my_string.scan( /\d+$/ ).first
+  def resolve_collision
+    username = self.username
+    nums = username.scan( /\d+$/ ).first || "1"
+    nums_len = nums.length
+    core_name = username.last =~ /\D/ ? username : username[0...nums_len*-1]
+    num = nums.to_i == 0 ? nums.to_i + 1 : nums.to_i
+    while UNIQUE_USERNAMES.include?("#{core_name}#{num}")
+      num += 1
+    end
+    new_name = "#{core_name}#{num.to_s}"
+  end
 
-  # Write a function that resolves all username collisions. E.g., two users with the username `foo` should become `foo` and `foo1`. The function accepts an optional "dry run" argument that will print the affected rows to the console, not commit the changes to the db.
+  def check_if_disallowed
+    DisallowedUsername.disallowed_usernames.include?(self.username.downcase)
+  end
 end
